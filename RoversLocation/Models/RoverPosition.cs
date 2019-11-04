e81e2c801d60cd3bca92ab19e1f49ed6fb39e640
pavanssvr <39695321+pavanssvr@users.noreply.github.com>
2019-11-04 20:48:29 +1100

    Update RoverPosition.cs

--- RoversLocation/Models/RoverPosition.cs
+++ RoversLocation/Models/RoverPosition.cs
@@ -6,11 +6,11 @@ namespace Models
     public class RoverPosition
     {
         [Required]
-        [StringLength(2, MinimumLength = 2)]
+        [StringLength(3, MinimumLength = 3)]
         public string Plateau { get; set; }
 
         [Required]
-        [StringLength(3, MinimumLength = 3)]
+        [StringLength(5, MinimumLength = 5)]
         public string Position { get; set; }
 
         [Required]
